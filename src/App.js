aaacea9d57258167b0e7863bb98fa7bb898295d5
선종권 <2ndprice@seonjong-gwon-ui-MacBookPro.local>
2021-07-06 11:21:00 +0900

    찐 완성

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
-import React, { useEffect } from 'react';
-import {
-  Switch,
-  Route,
-  useHistory
-} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
 
-import ChatPage from './components/ChatPage/ChatPage';
-import LoginPage from './components/LoginPage/LoginPage';
-import RegisterPage from './components/RegisterPage/RegisterPage';
+import ChatPage from "./components/ChatPage/ChatPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
-import firebase from './firebase';
+import firebase from "./firebase";
 
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  setUser,
-  clearUser
-} from './redux/actions/user_action';
+import { useDispatch, useSelector } from "react-redux";
+import { setUser, clearUser } from "./redux/actions/user_action";
 
 function App(props) {
   let history = useHistory();
   let dispatch = useDispatch();
-  const isLoading = useSelector(state => state.user.isLoading);
+  const isLoading = useSelector((state) => state.user.isLoading);
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged((user) => {
       // console.log('user', user)
       //로그인이 된 상태
       if (user) {
         history.push("/");
-        dispatch(setUser(user))
+        dispatch(setUser(user));
       } else {
         history.push("/login");
-        dispatch(clearUser())
+        dispatch(clearUser());
       }
-    })
-  }, [])
+    });
+  }, []);
 
   if (isLoading) {
-    return (
-      <div>
-        ...loading
-      </div>
-    )
+    return <div>...loading</div>;
   } else {
     return (
       <Switch>
